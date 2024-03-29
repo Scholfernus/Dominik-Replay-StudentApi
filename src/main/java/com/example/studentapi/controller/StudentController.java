eb1a893c16acce28554cece9c6e7008927407e0f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-29 21:43:53 +0100

    Student i TaskController

--- src/main/java/com/example/studentapi/controller/StudentController.java
+++ src/main/java/com/example/studentapi/controller/StudentController.java
@@ -3,9 +3,11 @@ package com.example.studentapi.controller;
 import com.example.studentapi.model.StudentModel;
 import com.example.studentapi.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -13,28 +15,51 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class StudentController {
-private final StudentService studentService;
 
+    private final StudentService studentService;
 
     @GetMapping("/students")
-    public String getStudentList(Model model){
+    public String getStudentList(Model model) {
         List<StudentModel> studentList = studentService.getStudentList();
         model.addAttribute("studentModel", studentList);
         return "persons/personList";
     }
-    @PostMapping("/addStudent")
-    public RedirectView postAddStudent(StudentModel student){
-    studentService.saveStudent(student);
-    return new RedirectView("/students");
-    }
 
     @GetMapping("/addStudent")
-    public String getAddStudent(){
+    public String getAddStudent() {
         return "persons/addNewPerson";
     }
-    @GetMapping("/editStudent")
-    public String getEditStudent(){
+
+
+    @PostMapping("/addStudent")
+    public RedirectView postAddStudent(StudentModel student) {
+        studentService.saveStudent(student);
+        return new RedirectView("/students");
+    }
+
+
+    @GetMapping("/editStudent/{id}")
+    public String getEditStudent(@PathVariable("id") Long id, Model model) {
+        StudentModel student = studentService.getStudentById(id);
+        model.addAttribute("studentModel", student);
         return "persons/editPerson";
     }
+
+
+    @PostMapping("/editStudent/{id}")
+    public RedirectView postEditStudent(@PathVariable("id") Long id, StudentModel student) {
+        studentService.saveEditStudent(student, id);
+        return new RedirectView("/students");
+    }
+
+
+    @PostMapping("/del/{id}")
+    public RedirectView delStudent(@PathVariable("id") Long id) {
+        studentService.deleteStudent(id);
+        return new RedirectView("/students");
+    }
+
+
 }
--- src/main/java/com/example/studentapi/controller/TaskController.java
+++ src/main/java/com/example/studentapi/controller/TaskController.java
@@ -1,16 +1,39 @@
 package com.example.studentapi.controller;
 
+import com.example.studentapi.model.StudentModel;
+import com.example.studentapi.model.TaskModel;
+import com.example.studentapi.service.StudentService;
+import com.example.studentapi.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
+    private final StudentService studentService;
+    private final TaskService taskService;
     @GetMapping("tasks")
-    public String getTaskList(){
+    public String getTaskList(Model model){
+       List<TaskModel> list = taskService.getTaskList();
+        model.addAttribute("taskModel", list);
         return "tasks/tasks";
     }
     @GetMapping("/addTask")
-    public String getAddTask(){
+    public String getAddTask(Model model){
+        List<StudentModel> list = studentService.getStudentList();
+        model.addAttribute("studentModel", list);
         return "tasks/addTask";
     }
+    @PostMapping("/addTask")
+    public RedirectView postAddTask(TaskModel task){
+        taskService.saveTask(task);
+        return new RedirectView("/tasks");
+    }
+    @
 }
