e5c9bfe93f31230e2ab0efa8a8614e00f4bcba27
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-30 19:05:34 +0100

    TaskRepository - dodatkowe metody z wykorzystaniem JpaRepository

--- src/main/java/com/example/studentapi/repository/TaskRepository.java
+++ src/main/java/com/example/studentapi/repository/TaskRepository.java
@@ -2,6 +2,20 @@ package com.example.studentapi.repository;
 
 import com.example.studentapi.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+    List<TaskModel> findByColor(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE TaskModel t SET t.description= :description WHERE t.id= :id")
+    int updateDescriptionById(@Param("id") Long id, @Param("description")String description);
+
+
 }
