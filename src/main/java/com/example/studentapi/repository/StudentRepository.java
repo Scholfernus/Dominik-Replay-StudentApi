8968796ed4b68ec664d6b76e53b8faa83ab3c2a1
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-31 22:01:34 +0200

    StudentRepository poprawa metod

--- src/main/java/com/example/studentapi/repository/StudentRepository.java
+++ src/main/java/com/example/studentapi/repository/StudentRepository.java
@@ -4,7 +4,24 @@ import com.example.studentapi.model.StudentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface StudentRepository extends JpaRepository<StudentModel,Long> {
+public interface StudentRepository extends JpaRepository<StudentModel, Long> {
+//    List<StudentModel> findStudentModelByLastName();
+
+    Optional<StudentModel> findById(Long id);
+
+    List<StudentModel> findDistinctByGitHubIsTrue();
+
+    List<StudentModel> findStudentModelByTasksIsTrue();
+
+    List<StudentModel> findStudentModelByFirstNameIsTrue();
+
+    List<StudentModel> findDistinctByGitHubIsFalse();
+
+    List<StudentModel> findStudentModelByTasksIsTrueAndGitHubIsTrue();
+
 
 }
